a1c084a817c5d7131b6565f3c676a2b95f4250f4
boredom101 <ymnewmark@gmail.com>
2018-09-03 17:44:43 -0400

    Initial Commit

---
+++ aura/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using AuraServiceLib;
+using AsRogAuraServiceLib;
+
+namespace aura
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var xml = System.IO.File.ReadAllText("profile.xml");
+            Console.WriteLine(xml);
+
+            var test = new AuraSdkManager();
+            test.Effect();
+            var test2 = new ServiceMediator();
+
+            Console.WriteLine(test.Devices.Item[0].Name);
+
+            Console.WriteLine(test.Devices.Count);
+
+            test2.SetProfile["Group"] = xml;
+
+            Console.WriteLine("Finished");
+            Console.Read();
+        }
+    }
+}
